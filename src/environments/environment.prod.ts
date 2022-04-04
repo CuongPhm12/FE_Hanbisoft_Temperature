e2bb7b32d6e8b085782937d35b2bd9893fb73ec0
liemnguyen <zovanser@gmail.com>
2022-04-04 09:00:46 +0700

    done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   // API_LOCAL
   // http://localhost:8080
   // https://temperature-hanbisoft.herokuapp.com
-  API_LOCAL: 'http://localhost:8080/api/',
-  API_LOCAL_USER: 'http://localhost:8080/',
+  API_LOCAL: 'https://temperature-hanbisoft.herokuapp.com/api/',
+  API_LOCAL_USER: 'https://temperature-hanbisoft.herokuapp.com/',
   production: true
 };
